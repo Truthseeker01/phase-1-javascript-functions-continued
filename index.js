3d95c7feae9237d96523380bf5bc1b2c0becc887
Ahmed Basamad <a.h.basamad.cn@gmail.com>
2024-03-13 22:56:30 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+};
+
+function mondayWork(work = "go to the office"){
+    return `This Monday, I will ${work}.`
+};
+
+function wrapAdjective(x = "*"){
+    return function (adjective = "spectial"){
+        return `You are ${x}${adjective}${x}!`
+        } 
+};
